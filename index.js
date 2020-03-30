bfbe31e603facc8bed1386c1b15b0116eb68cc3d
natashadihtiar <47831860+natashadihtiar@users.noreply.github.com>
2020-03-30 23:22:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,29 +9,29 @@ const counter = (initial = 0, step = 1) => {
         },
 
         reset() {
-            count = initial;
+            return count = initial;
         }
     };
 };
 
 const vkcounter = counter(4, 3);
 const fbcounter = counter(1, 1);
-const cleaner = counter();
+const cleaner = counter(0);
 
 const $VK = document.querySelector('#VK') ;
 const $FB =document.querySelector('#FB');
 const $Clean =document.querySelector('#Clean');
 
 $VK.addEventListener('click', () => {
-    console.log('VK:' + vkcounter.next());
+    vkcounter.next();
 });
 
 $FB.addEventListener('click', () => {
-    console.log('FB:' + fbcounter.next());
+    fbcounter.next();
 });
 
 $Clean.addEventListener('click',() => {
-    console.log(cleaner.reset()); 
+    vkcounter.reset(); 
+    fbcounter.reset(); 
 });
-
 
